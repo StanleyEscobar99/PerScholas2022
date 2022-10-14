c2ca8748c2fb54bf4d40327b82c12b02bb1d764a
Stanley Escobar <stanleyescobar@stanleys-air.mynetworksettings.com>
2022-10-14 17:12:47 -0400

    Added galaxy-far-far-away

--- W1/W1D3/conditionals.js
+++ W1/W1D3/conditionals.js
@@ -17,69 +17,69 @@
 
 // Examples of loose equals ==
 
-let x = "10";
+// let x = "10";
 
-if (x == 10) {
-  console.log("That is True!!");
-} else {
-  console.log("That is false");
-}
+// if (x == 10) {
+//   console.log("That is True!!");
+// } else {
+//   console.log("That is false");
+// }
 
 //Example of strictly equals ===
 
-let y = 10;
+// let y = 10;
 
-if (y === 10) {
-  console.log("That is true!!");
-} else {
-  console.log("It was false");
-}
+// if (y === 10) {
+//   console.log("That is true!!");
+// } else {
+//   console.log("It was false");
+// }
 
 // Exercises of IF statements
 
-let num = 0;
+// let num = 0;
 
-if (num > 0) {
-  console.log("It's positive");
-} else if (num < 0) {
-  console.log("It's negative");
-} else {
-  console.log("I'm zero");
-}
+// if (num > 0) {
+//   console.log("It's positive");
+// } else if (num < 0) {
+//   console.log("It's negative");
+// } else {
+//   console.log("I'm zero");
+// }
 
-let age = 17;
+// let age = 17;
 
-if (age >= 18) {
-  console.log("Acces Granted");
-} else {
-  console.log("Access denied");
-}
+// if (age >= 18) {
+//   console.log("Acces Granted");
+// } else {
+//   console.log("Access denied");
+// }
 
 // Nested if Statements
 
-if (true) {
-  if (true) {
-    console.log("nested if it is running");
-  } else {
-    console.log("second experssion if false");
-  }
-} else {
-  console.log("first experssion is false");
-}
+// if (true) {
+//   if (true) {
+//     console.log("nested if it is running");
+//   } else {
+//     console.log("second experssion if false");
+//   }
+// } else {
+//   console.log("first experssion is false");
+// }
 
 // Exercise:
 
-const integer = 100
+// const integer = 100
 
-if (integer > 0) {
-    if (integer > 100) {
-        console.log("Integer is greater than 100")
-    } else if (integer === 100){
-        console.log("It's 100")
-    }else {
-        console.log("Integer is less than 100")
-    }
-} else console.log("Integer is negative")
+// if (integer > 0) {
+//     if (integer > 100) {
+//         console.log("Integer is greater than 100")
+//     } else if (integer === 100){
+//         console.log("It's 100")
+//     }else {
+//         console.log("Integer is less than 100")
+//     }
+// } else console.log("Integer is negative")
 
 // Chained else/if statements
 // const integer = 21
